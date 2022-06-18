7f233465124b24e7fb6c33181208264c7ab4ea2b
Ignacio Useche <ignaciousecheparedes@gmail.com>
2022-06-18 10:46:58 -0400

    Add recursive function to go through the family Tree

--- fact_generator.py
+++ fact_generator.py
@@ -20,7 +20,7 @@ class FamilyNode:
         else:
             self.hasChildrens = True
 
-        self.makeFacts();
+        self.makeFacts()
     
     def makeFacts(self):
         if self.genre == 'F':
@@ -37,17 +37,14 @@ class FamilyNode:
 
         if self.hasPartner:
             self.facts.append(f'are_married("{self.name}","{self.partner}").')
-            self.facts.append('\n')
+            
             if self.genre == 'F':
                 self.facts.append(f'is_male("{self.partner}").')
                 self.facts += [f'is_father("{self.partner}","{x}").' for x in self.childrens]
             if self.genre == 'M':
                 self.facts.append(f'is_female("{self.partner}").')
                 self.facts += [f'is_mother("{self.partner}","{x}").' for x in self.childrens]
-            self.facts.append(f'are_married("{self.partner}",{self.name}").')
-            self.facts.append('\n')
-        else:
-            self.facts.append('\n')
+            self.facts.append(f'are_married("{self.partner}","{self.name}").')
 
     def printFacts(self):
        return '\n'.join(self.facts)
@@ -58,33 +55,57 @@ class FamilyNode:
                'partner: ' + self.partner + '\n' \
                'childs: ' + '\n' + '\n'.join(self.childrens)
 
+def treeTraveler(familyObjList, familyleaf):
+
+    familyObjList.append(
+        FamilyNode(
+            familyleaf['nombre'], 
+            familyleaf['genero'] ,
+            familyleaf['conyuge'], 
+            [x['nombre'] for x in familyleaf['hijos']]
+        )
+    )
+    for x in familyleaf['hijos']:
+        treeTraveler(familyObjList, x) 
+        
+
 if __name__=='__main__':
+
     familyDict = {}
     with open("arbol_genealogico.json") as file:
         familyDict= json.load(file)
 
     familyObjList = []
-    familyObjList.append(
-                FamilyNode(
-                familyDict['nombre'],
-                familyDict['genero'],
-                familyDict['conyuge'],
-                [x['nombre'] for x in familyDict['hijos']]))
+    rulesSet = set()
 
-    for sons in familyDict['hijos']:
-        familyObjList.append(
-        FamilyNode(
-        sons['nombre'],
-        sons['genero'],
-        sons['conyuge'],
-        [x['nombre'] for x in sons['hijos']]))
-
-        for grandsons in sons['hijos']:
-            familyObjList.append(
-            FamilyNode(
-            grandsons['nombre'],
-            grandsons['genero'],
-            grandsons['conyuge'],
-            [x['nombre'] for x in grandsons['hijos']]))
+    for x in familyDict:
+        treeTraveler(familyObjList,x)
                         
-    print('\n'.join([x.printFacts() for x in familyObjList]))
+    #print('\n'.join([x.printFacts() for x in familyObjList]))
+    
+    rulesList = []
+    for x in familyObjList:
+        rulesSet.update(x.facts)
+
+    rules ={
+        'is_male':[],
+        'is_female':[],
+        'is_father':[],
+        'is_mother':[],
+        'are_married':[]
+
+        }
+
+    for key in rules.keys():
+        for rule in rulesSet: 
+            if key in rule:
+                rules[key].append(rule)
+                print(rule)
+
+    with open('facts.pl','w') as facts_file:
+        for value in rules.values():
+            for fact in value:
+                facts_file.write(fact)
+                facts_file.write('\n')
+        
+        
--- facts.pl
+++ facts.pl
@@ -1,152 +1,146 @@
-is_female("Irma Silva").
-is_mother("Irma Silva","Simon Paredes").
-is_mother("Irma Silva","Marisabel Paredes").
-is_mother("Irma Silva","Yuli Paredes").
-is_mother("Irma Silva","Julio Paredes").
-is_mother("Irma Silva","Yelitza Paredes").
-are_married("Irma Silva","Ramon Ignacio Paredes").
-
-
-is_male("Ramon Ignacio Paredes").
-is_father("Ramon Ignacio Paredes","Simon Paredes").
-is_father("Ramon Ignacio Paredes","Marisabel Paredes").
-is_father("Ramon Ignacio Paredes","Yuli Paredes").
-is_father("Ramon Ignacio Paredes","Julio Paredes").
-is_father("Ramon Ignacio Paredes","Yelitza Paredes").
-are_married("Ramon Ignacio Paredes",Irma Silva").
-
-
-is_male("Simon Paredes").
-is_father("Simon Paredes","Marya Alejandra Paredes").
-is_father("Simon Paredes","Simon Andres Paredes").
-is_father("Simon Paredes","Maria Fernanda Paredes").
-
-
-is_female("Marya Alejandra Paredes").
-is_mother("Marya Alejandra Paredes","Gabriel").
-is_mother("Marya Alejandra Paredes","Maria Victoria").
-is_mother("Marya Alejandra Paredes","Maria Virginia").
-are_married("Marya Alejandra Paredes","Jose Enrique").
-
-
-is_male("Jose Enrique").
-is_father("Jose Enrique","Gabriel").
-is_father("Jose Enrique","Maria Victoria").
-is_father("Jose Enrique","Maria Virginia").
-are_married("Jose Enrique",Marya Alejandra Paredes").
-
-
-is_male("Simon Andres Paredes").
-
-
-is_female("Maria Fernanda Paredes").
-
-
-is_female("Marisabel Paredes").
-is_mother("Marisabel Paredes","Jose Ignacio Chuecos").
-is_mother("Marisabel Paredes","Carlos Luis Chuecos").
-is_mother("Marisabel Paredes","Julio Cesar Chuecos").
-are_married("Marisabel Paredes","Luis Chuecos").
-
-
+is_male("Rafael Ramirez").
 is_male("Luis Chuecos").
-is_father("Luis Chuecos","Jose Ignacio Chuecos").
-is_father("Luis Chuecos","Carlos Luis Chuecos").
-is_father("Luis Chuecos","Julio Cesar Chuecos").
-are_married("Luis Chuecos",Marisabel Paredes").
-
-
-is_male("Jose Ignacio Chuecos").
-
-
+is_male("Julio Paredes").
+is_male("Ramon Ignacio Paredes").
+is_male("Damian Useche").
+is_male("Ali Ramirez").
+is_male("Javier Useche").
+is_male("Crhirstiam Useche").
+is_male("Luis Javier Useche").
+is_male("David Useche").
+is_male("Simon Andres Paredes").
+is_male("Eric Becerra").
+is_male("Cesar Manuel Paredes").
+is_male("Danyelo Useche").
+is_male("Michell Useche").
+is_male("Jose Enrique").
 is_male("Carlos Luis Chuecos").
-is_father("Carlos Luis Chuecos","Amanda Caori").
-is_father("Carlos Luis Chuecos","Gemela Roja").
-is_father("Carlos Luis Chuecos","Gemela Rubia").
-are_married("Carlos Luis Chuecos","Analicia von shritz").
-
-
+is_male("Daniel Ignacio Useche").
+is_male("Jonathan Javier Useche").
+is_male("Gabriel").
+is_male("Simon Paredes").
+is_male("Julio Cesar Chuecos").
+is_male("Jose Ignacio Chuecos").
+is_male("Liam Useche").
+is_male("Daniel Useche").
+is_female("Panchadaniela Useche").
+is_female("Amanda Caori").
+is_female("Irma Silva").
+is_female("Sandra").
+is_female("Maria Victoria").
+is_female("Maria Virginia").
+is_female("Yulia Ramirez").
+is_female("Maria Fernanda Paredes").
+is_female("Natalia Lucia Becerra Useche").
+is_female("Danyelica Useche").
+is_female("Maria andrea Paredes").
+is_female("Marisabel Paredes").
+is_female("Marya Alejandra Paredes").
+is_female("Gemela Rubia").
+is_female("Yelitza Paredes").
+is_female("Aranxa").
+is_female("Milagros Galvis").
+is_female("Gemela Roja").
+is_female("Albadaniela Useche").
+is_female("Yuli Paredes").
+is_female("Sugeil Diaz").
+is_female("Alba Prato").
 is_female("Analicia von shritz").
-is_mother("Analicia von shritz","Amanda Caori").
+is_female("Monserrat").
+is_female("Juliana Paredes").
+is_female("Ana Maria Useche").
+is_female("Sofia").
+is_father("Daniel Useche","Michell Useche").
+is_father("Carlos Luis Chuecos","Gemela Rubia").
+is_father("Michell Useche","Crhirstiam Useche").
+is_father("Eric Becerra","Natalia Lucia Becerra Useche").
+is_father("Javier Useche","Ana Maria Useche").
+is_father("Crhirstiam Useche","Liam Useche").
+is_father("Luis Chuecos","Julio Cesar Chuecos").
+is_father("Carlos Luis Chuecos","Gemela Roja").
+is_father("Javier Useche","Jonathan Javier Useche").
+is_father("Michell Useche","Danyelo Useche").
+is_father("Jose Enrique","Gabriel").
+is_father("Julio Paredes","Cesar Manuel Paredes").
+is_father("Luis Chuecos","Jose Ignacio Chuecos").
+is_father("Ramon Ignacio Paredes","Yuli Paredes").
+is_father("Daniel Useche","Albadaniela Useche").
+is_father("Carlos Luis Chuecos","Amanda Caori").
+is_father("Simon Paredes","Marya Alejandra Paredes").
+is_father("Ramon Ignacio Paredes","Julio Paredes").
+is_father("Javier Useche","Luis Javier Useche").
+is_father("Michell Useche","Danyelica Useche").
+is_father("Daniel Useche","David Useche").
+is_father("Ali Ramirez","Rafael Ramirez").
+is_father("Jose Enrique","Maria Virginia").
+is_father("Ramon Ignacio Paredes","Yelitza Paredes").
+is_father("Ali Ramirez","Yulia Ramirez").
+is_father("Ramon Ignacio Paredes","Marisabel Paredes").
+is_father("Ramon Ignacio Paredes","Simon Paredes").
+is_father("Julio Paredes","Juliana Paredes").
+is_father("Michell Useche","Damian Useche").
+is_father("Daniel Useche","Javier Useche").
+is_father("Simon Paredes","Maria Fernanda Paredes").
+is_father("Luis Chuecos","Carlos Luis Chuecos").
+is_father("Julio Paredes","Maria andrea Paredes").
+is_father("Javier Useche","Daniel Ignacio Useche").
+is_father("Simon Paredes","Simon Andres Paredes").
+is_father("Jose Enrique","Maria Victoria").
+is_mother("Juliana Paredes","Aranxa").
+is_mother("Marya Alejandra Paredes","Gabriel").
+is_mother("Sugeil Diaz","Juliana Paredes").
+is_mother("Juliana Paredes","Monserrat").
+is_mother("Alba Prato","David Useche").
+is_mother("Marya Alejandra Paredes","Maria Victoria").
+is_mother("Sugeil Diaz","Cesar Manuel Paredes").
+is_mother("Yuli Paredes","Rafael Ramirez").
+is_mother("Sandra","Crhirstiam Useche").
+is_mother("Yulia Ramirez","Sofia").
+is_mother("Alba Prato","Michell Useche").
+is_mother("Irma Silva","Yelitza Paredes").
+is_mother("Sandra","Danyelica Useche").
+is_mother("Alba Prato","Albadaniela Useche").
+is_mother("Ana Maria Useche","Natalia Lucia Becerra Useche").
+is_mother("Irma Silva","Julio Paredes").
+is_mother("Albadaniela Useche","Panchadaniela Useche").
 is_mother("Analicia von shritz","Gemela Roja").
 is_mother("Analicia von shritz","Gemela Rubia").
-are_married("Analicia von shritz",Carlos Luis Chuecos").
-
-
-is_male("Julio Cesar Chuecos").
-
-
-is_female("Yuli Paredes").
-is_mother("Yuli Paredes","Yulia Ramirez").
-is_mother("Yuli Paredes","Rafael Ramirez").
-are_married("Yuli Paredes","Ali Ramirez").
-
-
-is_male("Ali Ramirez").
-is_father("Ali Ramirez","Yulia Ramirez").
-is_father("Ali Ramirez","Rafael Ramirez").
-are_married("Ali Ramirez",Yuli Paredes").
-
-
-is_female("Yulia Ramirez").
-is_mother("Yulia Ramirez","Sofia").
-
-
-is_male("Rafael Ramirez").
-
-
-is_male("Julio Paredes").
-is_father("Julio Paredes","Juliana Paredes").
-is_father("Julio Paredes","Maria andrea Paredes").
-is_father("Julio Paredes","Cesar Manuel Paredes").
-are_married("Julio Paredes","Sugeil Diaz").
-
-
-is_female("Sugeil Diaz").
-is_mother("Sugeil Diaz","Juliana Paredes").
-is_mother("Sugeil Diaz","Maria andrea Paredes").
-is_mother("Sugeil Diaz","Cesar Manuel Paredes").
-are_married("Sugeil Diaz",Julio Paredes").
-
-
-is_female("Juliana Paredes").
-is_mother("Juliana Paredes","Aranxa").
-is_mother("Juliana Paredes","Monserrat").
-
-
-is_female("Maria andrea Paredes").
-
-
-is_male("Cesar Manuel Paredes").
-
-
-is_female("Yelitza Paredes").
+is_mother("Marisabel Paredes","Julio Cesar Chuecos").
+is_mother("Sandra","Danyelo Useche").
+is_mother("Yelitza Paredes","Luis Javier Useche").
+is_mother("Sandra","Damian Useche").
+is_mother("Marisabel Paredes","Carlos Luis Chuecos").
+is_mother("Yelitza Paredes","Jonathan Javier Useche").
+is_mother("Irma Silva","Yuli Paredes").
+is_mother("Irma Silva","Simon Paredes").
+is_mother("Analicia von shritz","Amanda Caori").
 is_mother("Yelitza Paredes","Ana Maria Useche").
+is_mother("Alba Prato","Javier Useche").
+is_mother("Sugeil Diaz","Maria andrea Paredes").
 is_mother("Yelitza Paredes","Daniel Ignacio Useche").
-are_married("Yelitza Paredes","Javier Useche").
-
-
-is_male("Javier Useche").
-is_father("Javier Useche","Ana Maria Useche").
-is_father("Javier Useche","Daniel Ignacio Useche").
-are_married("Javier Useche",Yelitza Paredes").
-
-
-is_female("Ana Maria Useche").
-is_mother("Ana Maria Useche","Natalia Lucia Becerra Useche").
+is_mother("Marya Alejandra Paredes","Maria Virginia").
+is_mother("Irma Silva","Marisabel Paredes").
+is_mother("Marisabel Paredes","Jose Ignacio Chuecos").
+is_mother("Yuli Paredes","Yulia Ramirez").
+are_married("Luis Chuecos","Marisabel Paredes").
+are_married("Daniel Useche","Alba Prato").
+are_married("Ramon Ignacio Paredes","Irma Silva").
+are_married("Marya Alejandra Paredes","Jose Enrique").
+are_married("Julio Paredes","Sugeil Diaz").
 are_married("Ana Maria Useche","Eric Becerra").
-
-
-is_male("Eric Becerra").
-is_father("Eric Becerra","Natalia Lucia Becerra Useche").
-are_married("Eric Becerra",Ana Maria Useche").
-
-
-is_male("Daniel Ignacio Useche").
+are_married("Sugeil Diaz","Julio Paredes").
+are_married("Javier Useche","Yelitza Paredes").
+are_married("Sandra","Michell Useche").
+are_married("Carlos Luis Chuecos","Analicia von shritz").
+are_married("Yelitza Paredes","Javier Useche").
+are_married("Eric Becerra","Ana Maria Useche").
+are_married("Michell Useche","Sandra").
+are_married("Milagros Galvis","Daniel Ignacio Useche").
+are_married("Alba Prato","Daniel Useche").
+are_married("Yuli Paredes","Ali Ramirez").
 are_married("Daniel Ignacio Useche","Milagros Galvis").
-
-
-is_female("Milagros Galvis").
-are_married("Milagros Galvis",Daniel Ignacio Useche").
-
-
+are_married("Irma Silva","Ramon Ignacio Paredes").
+are_married("Jose Enrique","Marya Alejandra Paredes").
+are_married("Ali Ramirez","Yuli Paredes").
+are_married("Analicia von shritz","Carlos Luis Chuecos").
+are_married("Marisabel Paredes","Luis Chuecos").
